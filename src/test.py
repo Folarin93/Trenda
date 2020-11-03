c700c526427fd8e2ee9486754c3437695c6509cf
Christopher Joseph <alfonsoj93@hotmail.co.uk>
2020-11-03 11:12:02 +1100

    test files creation

---
+++ src/test.py
@@ -0,0 +1,17 @@
+import unittest
+from main import add
+
+class TestMathFunctions(unittest.TestCase):
+        def test_add(self):
+                result = add(1,5)
+                
+                self.assertEqual(result, 6, msg="Add function did not return 6 when given arguments 1 and 5")
+
+                result = add(-1,1)
+                
+                self.assertEqual(result, 0, msg="Add function did not return 0 when given arguments -1 and 1")
+
+                result = add(0,0)
+                
+                self.assertEqual(result, 0, msg="Add function did not return 0 when given arguments 0 and 0")
+
